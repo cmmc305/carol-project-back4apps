c25fd91b57f371568bde018559d28e424b82035a
bielgomes89 <bielgomes89@gmail.com>
2024-12-20 18:42:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import { name as appName } from './app.json';
 import CaseRequestForm from './src/CaseRequestForm';
 import { render } from 'react-dom';
 
-// Renderizar no navegador
+// Registrar como aplicativo web e nativo
 AppRegistry.registerComponent(appName, () => CaseRequestForm);
+
+// Renderizar no navegador
 const rootTag = document.getElementById('root') || document.createElement('div');
+document.body.appendChild(rootTag); // Garante que o root seja adicionado ao body
 render(<CaseRequestForm />, rootTag);
