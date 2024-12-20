01eeb8428759264bb95c9996911b8e7c29e00b39
bielgomes89 <bielgomes89@gmail.com>
2024-12-20 18:25:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import CaseRequestForm from './CaseRequestForm';
+import CaseRequestForm from './src/CaseRequestForm'; // Atualize o caminho aqui
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => CaseRequestForm);
