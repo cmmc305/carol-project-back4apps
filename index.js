3f8baf1cdf81b573ff5fb937ad711b0807454014
bielgomes89 <bielgomes89@gmail.com>
2024-12-20 18:37:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import CaseRequestForm from './src/CaseRequestForm'; // Atualize o caminho aqui
 import { name as appName } from './app.json';
+import CaseRequestForm from './src/CaseRequestForm';
+import { render } from 'react-dom';
 
+// Renderizar no navegador
 AppRegistry.registerComponent(appName, () => CaseRequestForm);
+const rootTag = document.getElementById('root') || document.createElement('div');
+render(<CaseRequestForm />, rootTag);
