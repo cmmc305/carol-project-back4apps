ca3af4587af3ef6f0fa62e032a6ea80e08a3fe9a
bielgomes89 <bielgomes89@gmail.com>
2024-12-20 18:59:48 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import { name as appName } from './app.json';
-import CaseRequestForm from './src/CaseRequestForm';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Opcional, dependendo se você usa CSS
+import App from './App';
 
-// Registrar como aplicativo web e nativo
-AppRegistry.registerComponent(appName, () => CaseRequestForm);
-
-// Renderizar no navegador
-const rootTag = document.getElementById('root') || document.createElement('div');
-document.body.appendChild(rootTag); // Garante que o root seja adicionado ao body
-render(<CaseRequestForm />, rootTag);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
