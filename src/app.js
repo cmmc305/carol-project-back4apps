8a734120a363d618566e1fb3e55800243cccd38a
bielgomes89 <bielgomes89@gmail.com>
2024-12-20 19:00:20 -0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CaseRequestForm from './CaseRequestForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Case Request Application</h1>
+      <CaseRequestForm />
+    </div>
+  );
+}
+
+export default App;
