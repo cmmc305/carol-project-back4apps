3c72b9469bbaf90a65ed009f40db98a2bf7158b8
bielgomes89 <bielgomes89@gmail.com>
2024-12-20 18:24:31 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import CaseRequestForm from './CaseRequestForm';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => CaseRequestForm);
