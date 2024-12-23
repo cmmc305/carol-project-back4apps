dc1031da96b1e388c6aa57a97fe651cd9e124ab7
bielgomes89 <bielgomes89@gmail.com>
2024-12-22 21:33:48 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ import CaseRequestForm from './components/CaseRequestForm';
 
 function App() {
   return (
-    <div>
-      <h1>Case Request Application</h1>
-      <CaseRequestForm />
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Case Request Application</h1>
+      </header>
+      <main className="app-main">
+        <CaseRequestForm />
+      </main>
+      <footer className="app-footer">
+        <p>&copy; {new Date().getFullYear()} Case Request Inc. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
