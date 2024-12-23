b662e9dd75d7083be80555ca1e06812a0208f56f
bielgomes89 <bielgomes89@gmail.com>
2024-12-22 21:05:25 -0300

    Update parseConfig.js

--- config/parseConfig.js
+++ config/parseConfig.js
@@ -1,1 +1,10 @@
+import Parse from 'parse/react-native';
 
+const APP_ID = '2NDD1ZpiL8vDCXeIOGfVlDCsYSDH1Pnl23Qazy17';
+const JAVASCRIPT_KEY = 'zdysUdPok7hl1X3hH6xnfbr48X35J0Fl734HjroY';
+const SERVER_URL = 'https://parseapi.back4app.com/';
+
+Parse.initialize(APP_ID, JAVASCRIPT_KEY);
+Parse.serverURL = SERVER_URL;
+
+export default Parse;
