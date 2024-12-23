9c422492d8b0e720ae46ead9696c6ba7a26cd9f0
bielgomes89 <bielgomes89@gmail.com>
2024-12-22 21:33:07 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import da nova API para renderização
 import './index.css';
 import App from './app';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // Criando o root
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
