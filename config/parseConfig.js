a8e67e20f6cb27d605c15b7a4acc99c914514f50
bielgomes89 <bielgomes89@gmail.com>
2024-12-22 21:34:45 -0300

    Update parseConfig.js

--- config/parseConfig.js
+++ config/parseConfig.js
@@ -1,4 +1,4 @@
-import Parse from 'parse/react-native';
+import Parse from 'parse';
 
 const APP_ID = '2NDD1ZpiL8vDCXeIOGfVlDCsYSDH1Pnl23Qazy17';
 const JAVASCRIPT_KEY = 'zdysUdPok7hl1X3hH6xnfbr48X35J0Fl734HjroY';
