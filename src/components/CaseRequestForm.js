edec71b13892c89959e73bf9705743dc7b2dd715
bielgomes89 <bielgomes89@gmail.com>
2024-12-26 16:06:50 -0300

    Update CaseRequestForm.js

--- src/components/CaseRequestForm.js
+++ src/components/CaseRequestForm.js
@@ -87,32 +87,147 @@ const CaseRequestForm = () => {
   };
 
   return (
-    <div className="form-wrapper">
-      <header className="form-header">
-        <img src={ReactLogo} alt="React Logo" className="react-logo" />
-        <h1>Case Request Form</h1>
-      </header>
-      <div className="form-container">
-        <div className="form-group">
-          <label>Requester Type:</label>
-          <input
-            type="text"
-            value={formData.requesterType}
-            onChange={(e) => handleInputChange('requesterType', e.target.value)}
-          />
-        </div>
-        {/* Outros campos */}
-        <div className="form-group">
-          <label>Transaction Proof Files:</label>
-          <input type="file" multiple onChange={(e) => handleFileUpload(e, setTransactionProofFiles)} />
-        </div>
-        <button className="submit-btn" onClick={handleSubmit}>
-          Submit
-        </button>
+    <div className="form-container">
+      <h1>Case Request Form</h1>    
+      <label>
+        Requester Email:
       </div>
-      <footer className="form-footer">
-        &copy; {new Date().getFullYear()} Your Company. All rights reserved.
-      </footer>
+      <div className="form-group">
+        <label>Requester Email:</label>
+        <input
+          type="email"
+          value={formData.requesterEmail}
+          onChange={(e) => handleInputChange('requesterEmail', e.target.value)}
+        />
+      </label>
+      <label>
+        Creditor Name:
+      </div>
+      <div className="form-group">
+        <label>Creditor Name:</label>
+        <input
+          type="text"
+          value={formData.creditorName}
+          onChange={(e) => handleInputChange('creditorName', e.target.value)}
+        />
+      </label>
+      <label>
+        Business Name:
+      </div>
+      <div className="form-group">
+        <label>Business Name:</label>
+        <input
+          type="text"
+          value={formData.businessName}
+          onChange={(e) => handleInputChange('businessName', e.target.value)}
+        />
+      </label>
+      <label>
+        Doing Business As:
+      </div>
+      <div className="form-group">
+        <label>Doing Business As:</label>
+        <input
+          type="text"
+          value={formData.doingBusinessAs}
+          onChange={(e) => handleInputChange('doingBusinessAs', e.target.value)}
+        />
+      </label>
+      <label>
+        Request Type:
+      </div>
+      <div className="form-group">
+        <label>Request Type:</label>
+        <select
+          value={formData.requestType}
+          onChange={(e) => handleInputChange('requestType', e.target.value)}
+@@ -143,63 +139,36 @@ const CaseRequestForm = () => {
+          <option value="Garnishment">Garnishment</option>
+          <option value="Release">Release</option>
+        </select>
+      </label>
+      <label>
+        Additional Entities:
+        <textarea
+          value={formData.additionalEntities}
+          onChange={(e) => handleInputChange('additionalEntities', e.target.value)}
+      </div>
+      <div className="form-group">
+        <label>Lien Balance:</label>
+        <InputMask
+          mask="99999.99"
+          value={formData.lienBalance}
+          onChange={(e) => handleInputChange('lienBalance', e.target.value)}
+        />
+      </label>
+      <label>
+        Address:
+        <input
+          type="text"
+          value={formData.address}
+          onChange={(e) => handleInputChange('address', e.target.value)}
+        />
+      </label>
+      <label>
+        State:
+        <input
+          type="text"
+          value={formData.state}
+          onChange={(e) => handleInputChange('state', e.target.value)}
+        />
+      </label>
+      <label>
+        City:
+        <input
+          type="text"
+          value={formData.city}
+          onChange={(e) => handleInputChange('city', e.target.value)}
+      </div>
+      <div className="form-group">
+        <label>Phone Number:</label>
+        <InputMask
+          mask="(999) 999-9999"
+          value={formData.phoneNumber}
+          onChange={(e) => handleInputChange('phoneNumber', e.target.value)}
+        />
+      </label>
+      <label>
+        Zipcode:
+        <input
+          type="text"
+      </div>
+      <div className="form-group">
+        <label>Zipcode:</label>
+        <InputMask
+          mask="99999-999"
+          value={formData.zipcode}
+          onChange={(e) => handleInputChange('zipcode', e.target.value)}
+        />
+      </label>
+      <label>
+        Phone Number:
+        <input
+          type="text"
+          value={formData.phoneNumber}
+          onChange={(e) => handleInputChange('phoneNumber', e.target.value)}
+        />
+      </label>
+      <label>
+        UCC Files:
+      </div>
+      <div className="form-group">
+        <label>UCC Files:</label>
+        <input type="file" multiple onChange={(e) => handleFileUpload(e, setUccFiles)} />
+      </label>
+      <label>
+        Transaction Proof Files:
+        <input type="file" multiple onChange={(e) => handleFileUpload(e, setTransactionProofFiles)} />
+      </label>
+      <button type="button" className="submit-btn" onClick={handleSubmit}>
+      </div>
+      <button className="submit-btn" onClick={handleSubmit}>
+        Submit
+      </button>
     </div>
   );
 };
