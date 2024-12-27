63666cc2ed1cc5ad54c6ec2e64795f0e1d2c4ed4
bielgomes89 <bielgomes89@gmail.com>
2024-12-27 00:01:46 -0300

    Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Parse from '../config/parseConfig';
-import './Login.css';
+import '../Login.css';
 
 const Login = () => {
   const [username, setUsername] = useState('');
