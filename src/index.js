3fd97ae4ce9c251f1ff9e3899902312cb8e42745
bielgomes89 <bielgomes89@gmail.com>
2024-12-27 01:37:39 -0300

    go

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { RouterProvider } from 'react-router-dom';
-import router from './routes';
 
 const router = createBrowserRouter(routes, {
   future: {
