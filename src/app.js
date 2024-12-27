a32967833278715ab1d2eefa0e051c02e7232b43
bielgomes89 <bielgomes89@gmail.com>
2024-12-26 23:56:54 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 import MainMenu from './components/MainMenu';
 import CaseRequestForm from './components/CaseRequestForm';
 import ListRequests from './components/ListRequests';
-import UpdateRequest from './components/UpdateRequest';
-import Register from './components/Register';
-import Login from './components/Login';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainMenu />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/main-menu" element={<MainMenu />} />
         <Route path="/create-request" element={<CaseRequestForm />} />
         <Route path="/list-requests" element={<ListRequests />} />
-        <Route path="/update-request" element={<UpdateRequest />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        {/* Outras rotas aqui */}
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
