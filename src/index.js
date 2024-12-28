d1dd87ba239c53b9c3c8b9e5664f63b8c1776ad3
bielgomes89 <bielgomes89@gmail.com>
2024-12-28 19:28:06 -0300

    go

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa o Bootstrap primeiro
 import './css/App.css'; // Seus estilos globais
-import './css/Sidebar.module.css'; // Estilos do Sidebar via CSS Modules
+import './components/Sidebar/Sidebar.module.css'; // Estilos do Sidebar via CSS Modules
 // Outros estilos específicos já são importados nos componentes correspondentes
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
