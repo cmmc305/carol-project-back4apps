f2f335e33886a27a4be3c595bc67e20f75bc4b41
bielgomes89 <bielgomes89@gmail.com>
2024-12-28 15:42:49 -0300

    go

--- src/components/ListRequests.js
+++ src/components/ListRequests.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Table, Container, Spinner, Button } from 'react-bootstrap';
 import Parse from '../config/parseConfig';
-import './css/App.css';
+import '../css/App.css';
 
 const ListRequests = () => {
   const [requests, setRequests] = useState([]);
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import './css/App.css';
+import '../css/App.css';
 
 const MainMenu = () => {
   const navigate = useNavigate();
