d34863aa0549022bf90e1750cdd16c5b524ab43e
bielgomes89 <bielgomes89@gmail.com>
2025-03-13 20:03:29 -0300

    Update parseConfig.js

--- src/config/parseConfig.js
+++ src/config/parseConfig.js
@@ -1,7 +1,7 @@
 import Parse from 'parse';
 
-const APP_ID = 'eiE9EYPU5UgRG6Aj1lcEhU9kzMZGbvR1Ew66gCfg';
-const JAVASCRIPT_KEY = 'gIpkEnNfIGBYDUn5bwbJaZMQMQD8Uoy9mBYwaoFt';
+const APP_ID = '9kdguQXyPc3Nhl46o5VdMhgXOgKIb4wgaGb1zUba';
+const JAVASCRIPT_KEY = 'JaDWJwxMUgde1QBR0fZsJUfa5V6aX9R7Ijpl1dUv';
 const SERVER_URL = 'https://parseapi.back4app.com/';
 
 Parse.initialize(APP_ID, JAVASCRIPT_KEY);
